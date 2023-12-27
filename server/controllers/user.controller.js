4d9b01a1fe1a0ac2c9f9812e29e1f5d783dfdb40
KirTer <kirill.ter@gmail.com>
2023-12-27 18:32:06 +0200

    refresh(refreshToken)

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -70,6 +70,13 @@ class UserControler {
 
    async refresh(req, res, next) {
       try {
+         const { refreshToken } = req.cookies;
+         const userData = await userService.refresh(refreshToken);
+         res.cookie('refreshToken', userData.refreshToken, {
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            // secure: true, // for httpS
+         });
       } catch (e) {
          next(e);
       }
--- server/services/token.service.js
+++ server/services/token.service.js
@@ -9,6 +9,26 @@ class TokenService {
       return { accessToken, refreshToken };
    }
 
+   validateAccessToken(token) {
+      try {
+         const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+         return userData;
+      } catch (e) {
+         return null;
+      }
+   }
+
+   validateRefreshToken(token) {
+      try {
+         const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+
+         return userData;
+      } catch (e) {
+         return null;
+      }
+   }
+
    async saveToken(userId, refreshToken) {
       const tokenData = await tokenModel.findOne({ user: userId });
       if (tokenData) {
@@ -21,6 +41,11 @@ class TokenService {
 
    async removeToken(refreshToken) {
       const tokenData = await tokenModel.deleteOne({ refreshToken });
+      return tokenData;
+   }
+
+   async findToken(refreshToken) {
+      const tokenData = await tokenModel.findOne({ refreshToken });
       return tokenData;
    }
 }
