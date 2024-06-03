ae3e406da194f40095e9602791e5f4bb30308244
KirTer <kirill.ter@gmail.com>
2024-06-03 17:52:55 +0300

    contact us

--- client/src/pages/homepage/index.tsx
+++ client/src/pages/homepage/index.tsx
@@ -122,7 +122,7 @@ const Homepage = () => {
                </h2>
 
                <div className={'grid grid-cols-1 sm:grid-cols-2 gap-x-16'}>
-                  <div className={' my-10'}>
+                  <div className={'my-10'}>
                      <h3 className={'text-3xl text-white font-light mb-3'}>Front End</h3>
 
                      <div
@@ -318,6 +318,160 @@ const Homepage = () => {
                   }>
                   Contact us
                </h1>
+            </div>
+
+            <div className={'grid grid-cols-1 sm:grid-cols-2 gap-x-16'}>
+               <div className={'text-white'}>
+                  <div className="grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-4">
+                     <div className="sm:col-span-2">
+                        <label
+                           htmlFor="firstName"
+                           className="block text-sm font-medium leading-6 text-white">
+                           First name
+                        </label>
+                        <div className="mt-2">
+                           <input
+                              type="text"
+                              name="firstName"
+                              id="firstName"
+                              autoComplete="given-name"
+                              className="block w-full bg-gray-600 ring-1 ring-gray-500 rounded-md border-0 p-2.5 text-white placeholder:text-gray-400 sm:leading-6"
+                           />
+                        </div>
+                     </div>
+
+                     <div className="sm:col-span-2">
+                        <label
+                           htmlFor="lastName"
+                           className="block text-sm font-medium leading-6 text-white">
+                           Last name
+                        </label>
+                        <div className="mt-2">
+                           <input
+                              type="text"
+                              name="lastName"
+                              id="lastName"
+                              autoComplete="family-name"
+                              className="block w-full bg-gray-600 ring-1 ring-gray-500 rounded-md border-0 p-2.5 text-white placeholder:text-gray-400 sm:leading-6"
+                           />
+                        </div>
+                     </div>
+
+                     <div className="sm:col-span-2">
+                        <label
+                           htmlFor="email"
+                           className="block text-sm font-medium leading-6 text-white">
+                           Email address
+                        </label>
+                        <div className="mt-2">
+                           <input
+                              id="email"
+                              name="email"
+                              type="email"
+                              autoComplete="email"
+                              className="block w-full bg-gray-600 ring-1 ring-gray-500 rounded-md border-0 p-2.5 text-white placeholder:text-gray-400 sm:leading-6"
+                           />
+                        </div>
+                     </div>
+
+                     <div className="sm:col-span-2">
+                        <label
+                           htmlFor="phone"
+                           className="block text-sm font-medium leading-6 text-white">
+                           Phone number
+                        </label>
+                        <div className="mt-2">
+                           <input
+                              id="phone"
+                              name="phone"
+                              type="tel"
+                              autoComplete="phone"
+                              className="block w-full bg-gray-600 ring-1 ring-gray-500 rounded-md border-0 p-2.5 text-white placeholder:text-gray-400 sm:leading-6"
+                           />
+                        </div>
+                     </div>
+
+                     <div className="col-span-full">
+                        <label
+                           htmlFor="country"
+                           className="block text-sm font-medium leading-6 text-white">
+                           Request subject
+                        </label>
+                        <div className="mt-2">
+                           <select
+                              id="country"
+                              name="country"
+                              autoComplete="country-name"
+                              className="block w-full bg-gray-600 ring-1 ring-gray-500 rounded-md border-0 p-2.5 text-white placeholder:text-gray-400 sm:leading-6">
+                              <option>Website quote</option>
+                              <option>Mobile application quote</option>
+                              <option>Customer service</option>
+                           </select>
+                        </div>
+                     </div>
+
+                     <div className="col-span-full">
+                        <label
+                           htmlFor="message"
+                           className="block text-sm font-medium leading-6 text-white">
+                           Message
+                        </label>
+                        <div className="mt-2">
+                           <textarea
+                              id="message"
+                              name="message"
+                              rows={3}
+                              className="block w-full bg-gray-600 ring-1 ring-gray-500 rounded-md border-0 p-2.5 text-white placeholder:text-gray-400 sm:leading-6"
+                              defaultValue={''}
+                           />
+                        </div>
+                     </div>
+
+                     <div className="col-span-full justify-end flex">
+                        <button
+                           onClick={() => {}}
+                           className={
+                              'content-center max-h-10 flex text-base items-center border-2 border-white text-white rounded-full pt-2.5 pb-2 px-4 group hover:bg-white hover:text-black hover:border-2'
+                           }>
+                           Send
+                           <SvgArrowRight
+                              className={'ms-3 h-4 fill-white group-hover:fill-black'}
+                           />
+                        </button>
+                     </div>
+                  </div>
+               </div>
+
+               <div className={'text-white'}>
+                  <label
+                     htmlFor="message"
+                     className="block text-sm font-medium leading-6 text-white">
+                     Our location
+                  </label>
+                  <div
+                     className={
+                        'mt-2 w-full h-56 bg-[url("/assets/images/map.jpg")] bg-center overflow-hidden mb-10 bg-gray-600 ring-1 ring-gray-500 rounded-md border-0'
+                     }
+                  />
+                  <div>
+                     Migdal Sonol
+                     <br />
+                     52 Menachem Begin,
+                     <br />
+                     Tel Aviv, 67137
+                     <br />
+                     <br />
+                     Tel: 052-8287009
+                     <br />
+                     Email: contact@01combine.com
+                  </div>
+               </div>
+            </div>
+         </section>
+
+         <section className={'w-full px-4 mt-48 bg-gray-400'}>
+            <div className={'max-w-6xl mx-auto'}>
+               <h1>FOOTER</h1>
             </div>
          </section>
       </div>
