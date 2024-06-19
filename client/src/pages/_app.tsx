613211dbbcdcbda8530114dcf585752527c8b4ac
KirTer <kirill.ter@gmail.com>
2024-06-19 17:26:18 +0300

    RTL layout

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { LayoutMain, Navbar } from '@/components/';
 import AppHeadHtml from '@/components/AppHeadHtml';
 import { appWithTranslation } from 'next-i18next';
 import './globals.css';
 import '../locales/i18n';
+import { useRouter } from 'next/router';
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+   const router = useRouter();
+
+   useEffect(() => {
+      const dir = router.locale === 'he' ? 'rtl' : 'ltr';
+      // @ts-ignore
+      document.querySelector('body').setAttribute('dir', dir);
+   }, [router.locale]);
+
    return (
       <>
          <AppHeadHtml />
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,41 +1,72 @@
+import Document, {
+   DocumentContext,
+   DocumentInitialProps,
+   Head,
+   Html,
+   Main,
+   NextScript,
+} from 'next/document';
 import { Footer } from '@/components';
-import { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 
-export default function _document() {
-   return (
-      <Html className={'h-full'} dir={'ltr'} lang="en">
-         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link
-               rel="preconnect"
-               href="https://fonts.gstatic.com"
-               crossOrigin={'use-credentials'}
-            />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link
-               rel="preconnect"
-               href="https://fonts.gstatic.com"
-               crossOrigin={'use-credentials'}
-            />
-            {/*TODO убрать лишние шрифты*/}
-            <link
-               href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-               rel="stylesheet"
-            />
-         </Head>
+class MyDocument extends Document {
+   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+      const originalRenderPage = ctx.renderPage;
 
-         <body className={'h-full bg-bgColored bg-cover bg-black'}>
-            {/*<div*/}
-            {/*   className={*/}
-            {/*      'absolute opacity-40 top-0 left-0 bottom-0 font-openSans slashed-zero text-gray-800 z-0 text-xs text-pretty text-center'*/}
-            {/*   }>*/}
-            {/*   {backgrString}*/}
-            {/*</div>*/}
-            <Main />
-            <NextScript />
-            <Footer />
-         </body>
-      </Html>
-   );
+      // Run the React rendering logic synchronously
+      ctx.renderPage = () =>
+         originalRenderPage({
+            // Useful for wrapping the whole react tree
+            enhanceApp: (App) => App,
+            // Useful for wrapping in a per-page basis
+            enhanceComponent: (Component) => Component,
+         });
+
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return initialProps;
+   }
+
+   render() {
+      const { locale } = this.props.__NEXT_DATA__;
+      const dir = locale === 'he' ? 'rtl' : 'ltr';
+      return (
+         <Html className={'h-full'} dir={dir} lang={locale}>
+            <Head>
+               <link rel="preconnect" href="https://fonts.googleapis.com" />
+               <link
+                  rel="preconnect"
+                  href="https://fonts.gstatic.com"
+                  crossOrigin={'use-credentials'}
+               />
+               <link rel="preconnect" href="https://fonts.googleapis.com" />
+               <link
+                  rel="preconnect"
+                  href="https://fonts.gstatic.com"
+                  crossOrigin={'use-credentials'}
+               />
+               {/*TODO убрать лишние шрифты*/}
+               <link
+                  href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+                  rel="stylesheet"
+               />
+            </Head>
+
+            <body className={'h-full bg-bgColored bg-cover bg-black'}>
+               {/*<div*/}
+               {/*   className={*/}
+               {/*      'absolute opacity-40 top-0 left-0 bottom-0 font-openSans slashed-zero text-gray-800 z-0 text-xs text-pretty text-center'*/}
+               {/*   }>*/}
+               {/*   {backgrString}*/}
+               {/*</div>*/}
+               <Main />
+               <NextScript />
+               <Footer />
+            </body>
+         </Html>
+      );
+   }
 }
+
+export default MyDocument;
