0e45ac79cab98f2712fd1e14ca12e4a2bfe0ec5b
KirTer <kirill.ter@gmail.com>
2024-06-24 14:43:38 +0300

    <head> ...

--- client/src/components/PageHead/PageHead.tsx
+++ client/src/components/PageHead/PageHead.tsx
@@ -1,16 +1,14 @@
+import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
 import React from 'react';
 
 interface PageHeadProps {
-   title?: string;
    pageName?: string;
 }
 
-const PageHead: React.FC<PageHeadProps> = ({
-   title = '0/1 Combine — Mobile App and Web Development Experts',
-   pageName,
-}) => {
-   const fullTitle = pageName ? `${pageName} — 0/1 Combine` : title;
+const PageHead: React.FC<PageHeadProps> = ({ pageName }) => {
+   const { t } = useTranslation('common');
+   const fullTitle = pageName ? `${pageName} — 0/1 Combine` : t('headTitle');
 
    return (
       <Head>
